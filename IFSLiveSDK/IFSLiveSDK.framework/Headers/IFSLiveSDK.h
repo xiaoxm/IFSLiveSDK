8a592f73f27081028868d18ae2a04e27a1f957ee
herui <herui@ifensi.com>
2020-04-22 10:17:28 +0800

    update

---
+++ IFSLiveSDK/IFSLiveSDK.framework/Headers/IFSLiveSDK.h
@@ -0,0 +1,11 @@
+//
+//  IFSLiveSDK.h
+//  IFSLiveSDK
+//
+//  Created by herui on 2020/2/27.
+//  Copyright © 2020 ifensi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "IFSLiveSDKConfig.h"
+#import "IFSLiveListViewController.h"
